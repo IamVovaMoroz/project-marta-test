2c14c93ee6814abbe623b4108da0f097c9bb50e2
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-12-18 19:52:39 +0100

    add

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // api: "http://127.0.0.1:1337",
-  api: "https://marta-back.onrender.com",
+  api: "http://127.0.0.1:1337",
+  // api: "https://marta-back.onrender.com",
 };
 
 export default config;
